f1ac060ffa0a8c6dd337960fe3a7a3022c6b7cfb
Destiny Boone <destaj.db@gmail.com>
2022-10-27 16:44:36 -0400

    assigning an alias to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,8 @@ import { Component, Input, OnInit, } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string}; //@Input decorator now exposes this component's properties to it's parents
+  @Input('srvElement') element: {type: string, name: string, content: string}; //@Input decorator now exposes this component's properties to it's parents
+  // inside the parenthesis we have put an alias so we use that name instead to reference this property
 
   constructor() { }
 
