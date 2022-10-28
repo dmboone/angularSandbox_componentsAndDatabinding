52ac23142c1ed807b4f0e569774a56e6e3bfe09e
Destiny Boone <destaj.db@gmail.com>
2022-10-28 11:10:00 -0400

    view encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation, } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // this means that any styles applied to this component in its css file will be applied globally to all components; the default would be .Emulated instead of .None
+  // ViewEncapsulation.Emulated is the default which would mean styles in the corresponding css file would only apply to that specific component
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string}; //@Input decorator now exposes this component's properties to it's parents
