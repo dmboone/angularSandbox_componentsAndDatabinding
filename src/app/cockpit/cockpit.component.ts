1931b53c5fcebf7d01d494bbf0d2fee5a673d28d
Destiny Boone <destaj.db@gmail.com>
2022-10-28 11:40:38 -0400

    getting access to the template and DOM with @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,7 +9,10 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>(); // we have just turned these first two properties into events
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>(); // added the alias 'bpCreated' so this is how you must now reference the event from the outside
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef; // gives direct access to elements in our DOM in our template based on reference name serverContentInput
+  // must put {static: true} if you plan on accessing the selected element inside of ngOnInit()
+  // do not use view child to change elements in the DOM; there are better ways
 
   constructor() { }
 
@@ -19,14 +22,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement){
     this.serverCreated.emit({ // emitting our created events from above and passing the data
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
   onAddBlueprint(nameInput: HTMLInputElement){
     this.blueprintCreated.emit({ // emitting our created events from above and passing the data
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 }
